98030755cadfd5cac0833ed276bea4fc607205e9
Yunus Emre Sarıca <68511425+saricayemre@users.noreply.github.com>
2023-08-21 14:44:19 +0300

    Add files via upload

---
+++ loginpage/loginpage/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace loginpage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.groupbox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupbox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(100, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.LightBlue;
+            this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_username.Location = new System.Drawing.Point(25, 19);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(150, 20);
+            this.txt_username.TabIndex = 1;
+            this.txt_username.Text = "Kullanıcı Adı";
+            this.txt_username.Click += new System.EventHandler(this.txt_username_Click);
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.LightBlue;
+            this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txt_password.Location = new System.Drawing.Point(25, 45);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.PasswordChar = '•';
+            this.txt_password.Size = new System.Drawing.Size(150, 20);
+            this.txt_password.TabIndex = 2;
+            this.txt_password.Text = "Parola";
+            this.txt_password.Click += new System.EventHandler(this.txt_password_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(100, 337);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.Color.LightBlue;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_login.Location = new System.Drawing.Point(25, 72);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(150, 23);
+            this.btn_login.TabIndex = 4;
+            this.btn_login.Text = "GİRİŞ";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // groupbox1
+            // 
+            this.groupbox1.Controls.Add(this.txt_username);
+            this.groupbox1.Controls.Add(this.btn_login);
+            this.groupbox1.Controls.Add(this.txt_password);
+            this.groupbox1.Location = new System.Drawing.Point(45, 178);
+            this.groupbox1.Name = "groupbox1";
+            this.groupbox1.Size = new System.Drawing.Size(200, 111);
+            this.groupbox1.TabIndex = 5;
+            this.groupbox1.TabStop = false;
+            this.groupbox1.Text = "Admin Giriş";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(284, 461);
+            this.Controls.Add(this.groupbox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Login Page";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupbox1.ResumeLayout(false);
+            this.groupbox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.GroupBox groupbox1;
+    }
+}
+
---
+++ loginpage/loginpage/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace loginpage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void login()
+        {
+            SqlConnection con;
+            SqlCommand cmd;
+            SqlDataReader dr;
+            string query = "SELECT * FROM admin where username=@username AND password=@password";
+            con = new SqlConnection("server=.; Initial Catalog=dbname;Integrated Security=SSPI");
+            cmd = new SqlCommand(query, con);
+            cmd.Parameters.AddWithValue("@username", txt_username.Text);
+            cmd.Parameters.AddWithValue("@password", txt_password.Text);
+            con.Open();
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                MessageBox.Show("Tebrikler! Başarılı bir şekilde giriş yaptınız.","Giriş Başarılı", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı adını ve şifrenizi kontrol ediniz.","Hatalı Giriş",MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+            }
+            con.Close();
+        }
+
+
+
+        private void txt_username_Click(object sender, EventArgs e)
+        {
+            txt_username.Clear();
+        }
+
+        private void txt_password_Click(object sender, EventArgs e)
+        {
+            txt_password.Clear();
+            //txt_password.PasswordChar = '*';
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            login();
+        }
+    }
+}
